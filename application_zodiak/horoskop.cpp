3687f214869aff53fd27305ca8735d37779ed9e7
ArturGrzegrzolka <ArturGrzegrzolka@gmail.com>
2020-01-15 20:24:04 +0100

    dodanie wyznaczania dnia tygodnia
    dodanie wyznaczania dnia tygodnia i kilka drobnych zmian ustalonych podczas calla

--- application_zodiak/horoskop.cpp
+++ application_zodiak/horoskop.cpp
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 #include <cstdlib>
 #include <windows.h> 
-#include <sstream>
 
 using namespace std;
 
@@ -26,9 +25,9 @@ int vl_length=0, vl_rok	= v_year;
 		vl_length++;
 	} while (vl_rok > 0);
 
-	if (   v_year		>	st.wYear//jesli podany rok jest wiekszy niz biezacy rok
-		|| st.wYear-100	>	v_year//lub jesli biezacy rok - 100 lat jest wiekszy niz podany rok
-		|| vl_length	!=	4)//lub ilosc cyfr w roku jest <> 4
+	if (   v_year		>	st.wYear 	//jesli podany rok jest wiekszy niz biezacy rok
+		|| st.wYear-100	>	v_year 		//lub jesli biezacy rok - 100 lat jest wiekszy niz podany rok
+		|| vl_length	!=	4)   		//lub ilosc cyfr w roku jest <> 4 
 		{	cout<< "Podales niewlasciwy rok urodzenia. Sprobuj jeszcze raz" << endl;
 			cout << "Podany rok to: " << v_year<< endl;
 			cout << "dlugosc liczby:" <<vl_length<< endl;
@@ -59,91 +58,125 @@ bool f_CzyJestMountUrodzenia ( int v_mount  )
 
 //-------------------------------------------------------------------------------------
 bool f_CzyJestDayUrodzenia ( int v_day, int v_mount, int v_year )
-{
-int vl_length = 0;
-string vl_tab_31="01,03,05,07,08,10,12";
-string vl_tab_30="04,06,09,11";
-string vl_tab_28="02";
-string vl_day;
-stringstream vl_ss;
+{	
+	int vl_max_day, i;	
+	int vl_tab_31[7]={1,3,5,7,8,10,12};
+	int vl_tab_30[4]={4,6,9,11};
+	int vl_tab_28[1]={2};
+
 	cin.clear();
-	cin.sync();
+	cin.sync();	
 
-	//zamiana int-a na string
-	vl_ss << v_day;
-	vl_day=vl_ss.str();
-	vl_length=vl_day.length();
+	cout<<"dni: 	"	<<v_day		<<endl;
+	cout<<"miesiac: "	<<v_mount	<<endl;
+	cout<<"rok: 	"	<<v_year	<<endl;
+	
 
-	if (vl_length<2 || vl_length>2)
-		{	cout<< "Podales niewlasciwa wartosc. Sprobuj jeszcze raz" << endl;
-			getch();
-			return false;
+	//jesli miesiac to luty
+	if (v_mount=2 )	
+	{	//okreslenie czy rok byl przestepny
+		if((v_year%4==0 && !(v_year%100==0)) || (v_year%400==0))
+			{            vl_max_day = 29;        }
+		else
+			{vl_max_day = 28;
+			 cout<< "ilosc dni w lutym: "<<vl_max_day<<endl;		}
+
+		for( i=0; i < sizeof(vl_tab_28)/sizeof(int); i++)
+		if( vl_tab_28[i] == v_mount && (v_day>0 && v_day<vl_max_day+1))
+		        return true;
+	}	
+	else
+		// jesli miesiac ma 31 dni
+		for( i=0; i < sizeof(vl_tab_31)/sizeof(int); i++)
+		if( vl_tab_28[i] == v_mount && (v_day>0 && v_day<31+1))
+		        return true;
+	    else 
+		{//jesli miesiac ma 30 dni
+			for( i=0; i < sizeof(vl_tab_30)/sizeof(int); i++)
+			if( vl_tab_28[i] == v_mount && (v_day>0 && v_day<30+1))
+			        return true;
 		}
-	// jesli miesiac ma 31 dni
-	size_t znalezionaPozycja = vl_tab_31.find( v_mount );
-	if( znalezionaPozycja != std::string::npos && (v_day>0 && v_day<32))
-			return true;
-	else
-			return false;
-	// jesli miesiac ma 30 dni
-	znalezionaPozycja = vl_tab_30.find( v_mount );
-	if( znalezionaPozycja != std::string::npos && (v_day>0 && v_day<31))
-			return true;
-	else
-			return false;
-	// jesli miesiac to luty
-	znalezionaPozycja = vl_tab_28.find( v_mount );
-	if( znalezionaPozycja != std::string::npos && (v_day>0 && v_day<31))
-			return true;
-	else
-			return false;
+	return false;
+}
+//-------------------------------------------------------------------------------------
+string f_OdgadnijDzienUrodzin ( int v_day, int v_mount, int v_year )
+{
+    time_t czas;
+    struct tm * data;
+	const char * dzien_tygodnia[] = { "Niedziela", "Poniedzialek","Wtorek", "Sroda", "Czwartek", "Piatek", "Sobota" };
 
-	znalezionaPozycja = vl_tab_31.find( v_day );
-	if( znalezionaPozycja != std::string::npos )
-			return true;
-	else if (size_t znalezionaPozycja = vl_tab_30.find( v_day ))
-			return true;
-	else if (size_t znalezionaPozycja = vl_tab_28.find( v_day ))
-			return true;
-
-	return true;
+    time( & czas );
+    data = localtime( & czas );
+    data->tm_year = v_year - 1900;
+    data->tm_mon = v_mount - 1;
+    data->tm_mday = v_day;
+   
+    mktime( data );
+    return dzien_tygodnia[ data->tm_wday ];
 }
 
+
 //-------------------------------------------------------------------------------------
-int f_OkreslenieDniaTygodnia ()
+int f_PodajDane ()
 {
 	int v_day, v_mount, v_year;
-	bool v_czyYear = false, v_czyMount = false,v_czyDay = false;
+	bool v_czyPoprawneDane = false;
+	string v_imie, v_dzienUrodzin;
+	
 	do
 	{ 	cin.clear();
 		cin.sync();
 		system("cls");
 		cout<<"podaj rok urodzenia" <<endl;
 		cin>>v_year;
-		v_czyYear = f_CzyJestYearUrodzenia( v_year ) ;
-	} while (v_czyYear == false);
+		v_czyPoprawneDane = f_CzyJestYearUrodzenia( v_year ) ;
+	} while (v_czyPoprawneDane == false);
 
+	v_czyPoprawneDane = false;
 	do
 	{ 	cin.clear();
 		cin.sync();
 		cout<<"podaj miesiac urodzenia" <<endl;
 		cin>>v_mount;
-		v_czyMount = f_CzyJestMountUrodzenia( v_mount ) ;
-	} while (v_czyMount == false);
-
+		v_czyPoprawneDane = f_CzyJestMountUrodzenia( v_mount ) ;
+	} while (v_czyPoprawneDane == false);
+	
+	//cout<< "true: "<< true<<endl;
+	//cout<< "false:"<< false<<endl;
+	//cout << "v_czyYear:  "<<v_czyYear << endl;
+	//cout << "v_czyMount: "<<v_czyMount << endl;	
+	
+	v_czyPoprawneDane = false;
 	do
 	{ 	cin.clear();
 		cin.sync();
 		cout<<"podaj dzien urodzenia" <<endl;
 		cin>>v_day;
-		v_czyDay = f_CzyJestDayUrodzenia( v_day, v_mount, v_year) ;
-	} while (v_czyDay== false);
+		v_czyPoprawneDane = f_CzyJestDayUrodzenia( v_day, v_mount, v_year) ;	
+		//cout << "v_czyDay: "<<v_czyDay << endl;
+	} while (v_czyPoprawneDane== false);
+	
+	v_czyPoprawneDane = true; //po zrobieniu walidacji zmien na false
+	do
+	{ 	cin.clear();
+		cin.sync();
+		cout<<"Podaj imie" <<endl;
+		cin>>v_imie;
+		//v_czyPoprawneDane = f_Czyimie( v_imie) ;	//tu powinno byc wywolanie funkcji do walidacji imienia czy np. user nie wpisal 1234... pamietaj ze jest cos takiego jak zakres ... (w przypadku skracania linkow bylo to uzywane.)
+		//dodalbym jeszcze initcap-a niezaleznie od tego co user wpisal.
+	} while (v_czyPoprawneDane== false);
+
+	v_dzienUrodzin = f_OdgadnijDzienUrodzin(v_day, v_mount, v_year);
+    
+	cout << "Czesc "<< v_imie <<". "<<endl;
+	cout << "Data Twoich narodzin to: " <<v_day<<"/"<<v_mount<<"/"<<v_year<<endl;
+	cout << "Dzien twoich urodzin to: " << v_dzienUrodzin <<endl;
+    
+    getch();
 }
 
 int main()
-{
-
-	//srand(time ( 0 ) );
+{	//srand(time ( 0 ) );
 	char v_znak;
 	do
 	{
@@ -165,8 +198,8 @@ int main()
 		cout <<"9. Wyznaczenie fazy ksiezyca w dniu urodzin"								<< endl;
 		cout <<"10.Import danych do glownej bazy danych"									<< endl;
 		cout <<"11.Funkcja zwracajaca pelna zawartosc bazy danych"							<< endl;
-		cout <<"12.Aggregaty na bazie danych"												<< endl << endl << endl;
-		cout <<"Aby rozpoczac korzystanie z programu wcisnij 1, aby zakonczyc dzialanie programu wcisnij W."<< endl << endl << endl;
+		cout <<"12.Agregaty na bazie danych"												<< endl<< endl;
+		cout <<"Aby rozpoczac korzystanie z programu wcisnij 1, aby zakonczyc dzialanie programu wcisnij W."<< endl << endl;
 		cout<<"Dokonaj wyboru :";
 
 		v_znak = getchar();
@@ -182,23 +215,23 @@ int main()
 		cin.sync();
 		system("cls");
 		cout <<"********************* MENU *********************"	<< endl;
-		cout << "1. okreslenie dnia tygodnia z daty urodzenia	"	<< endl;
-		cout << "2. okreslenia plci na podstawie imienia		"	<< endl;
-		cout << "3. Wyznaczenie wieku uzytkownika				"	<< endl;
-		cout << "4. rozpoznanie znaku zodiaku					"	<< endl;
-		cout << "5. wyswietl horoskop							"	<< endl;
-		cout << "6. Wyznaczenie fazy ksiezyca w dniu urodzin	"	<< endl;
-		cout << "7. wyswietl historie							"	<< endl;
-		cout << "8. dodaj dane z pliku							"	<< endl;
-		cout << "9. poka¿ agregaty								"	<< endl;
-		cout << "W. wyjscie z programu							" << endl;
+		cout << "1. Okreslenie dnia tygodnia z daty urodzenia	"	<< endl;
+		cout << "   Okreslenia plci na podstawie imienia		"	<< endl;
+		cout << "   Wyznaczenie wieku uzytkownika				"	<< endl;
+		cout << "   Rozpoznanie znaku zodiaku					"	<< endl;
+		cout << "   Wyswietl horoskop							"	<< endl;
+		cout << "   Wyznaczenie fazy ksiezyca w dniu urodzin	"	<< endl;
+		cout << "2. Wyswietl historie							"	<< endl;
+		cout << "3. Dodaj dane z pliku							"	<< endl;
+		cout << "4. Pokaz agregaty								"	<< endl;
+		cout << "W. Wyjscie z programu							" << endl;
 		cout<<"Wybieram : ";
 
 		v_znak = getchar();
 		switch (v_znak)
 		{
 			case '1':
-				f_OkreslenieDniaTygodnia();
+				f_PodajDane();
 				break;
 			case '2':
 				//f_znajdz_url();
