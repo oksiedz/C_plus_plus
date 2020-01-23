d2953aea230072dc5a9a012dd86567d7371a87c7
ArturGrzegrzolka <ArturGrzegrzolka@gmail.com>
2020-01-23 22:43:05 +0100

    dodanie importu pliku
    ale wystepuja problemy z kodowanie polskich znakow.
    bede jeszcze nad tym pracowal

--- application_zodiak/horoskop.cpp
+++ application_zodiak/horoskop.cpp
@@ -26,8 +26,11 @@ class Cechy
 	string faza_ksiezyca;
 };
 
-Cechy Funboy; //obiekt Funboy opisany przez klase Cechy
-Cechy *pFunboy; //deklaracja wskaznika do obiektu
+Cechy Funboy; Tworzymy dynamiczna tablice obiektow
+Cechy tFunboy[10];			// Tworzymy tablice obiektow
+Cechy oFunboy; 				//obiekt oFunboy opisany przez klase Cechy
+Cechy *pFunboy; 			//deklaracja wskaznika do obiektu
+Cechy *ptFunboy = &tFunboy[0];		//przypisanie wskaźnikowi wartosci
 
 //-------------------------------------------------------------------------------------
 bool f_CzyJestYearUrodzenia ( int v_year )
@@ -53,7 +56,7 @@ int vl_length=0, vl_rok	= v_year;
 			return false;
 		}
 	else if (v_year == st.wYear )		//jesli podany rok jest rowny biezacemu
-		{	cout<< "Jetes za mlody na korzystanie z komputera." << endl;
+		{	cout<< "Jetes za młody na korzystanie z komputera." << endl;
 			getch();
 			exit(0);
 			return false;
@@ -65,7 +68,7 @@ bool f_CzyJestMountUrodzenia ( int v_month )
 {	cin.clear();
 	cin.sync();
 	if (v_month>12 || v_month<1)		//jesli podany miesiac nie istnieje
-		{	cout<< "Podałes niewłasciwy miesiśc urodzenia. Spróbuj jeszcze raz" << endl;
+		{	cout<< "Podałeś niewłaściwy miesiąc urodzenia. Spróbuj jeszcze raz" << endl;
 			getch();
 			return false;
 		}
@@ -165,32 +168,33 @@ string f_OdgadnijDzienUrodzin ( int v_day, int v_month, int v_year )
 	return dzien_tygodnia[ data->tm_wday ];
 }
 //-------------------------------------------------------------------------------------
+char *f_PolskieZnaki(char *str)
+  {
+   static char buff[256];
+   char cp[]="\245\206\251\210\344\242\230\276\253\244\217\250\235\343\340\227\275\215ąćęłńóśżźĄĆĘŁŃÓŚŻŹ";
+   if(strlen(str)>=sizeof(buff)) return str;
+   char *bf=buff;
+   while(*str)
+     {
+      char *pos=strchr(cp+18,*str);
+      *(bf++)=pos?*(pos-18):*str;
+      ++str;
+     }
+   *bf=0;
+   return buff;
+  }
+//-------------------------------------------------------------------------------------
 bool f_CzyPoprawneImie ( string v_name )
 {
 	int vl_NameLength, i, j, vl_ZnalezionaLiterka=0;
-	char tl_ZbiorZnakow[53]={"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
-
-	vl_NameLength = v_name.length();
-	//cout<<"vl_NameLength"<<vl_NameLength<<endl;
-	for (i=0; i<vl_NameLength; i++)
-	{
-		//cout<<"litera : "<< v_name[i] <<endl;
-		for (j=0; j<52; j++)
-		{
-			if (v_name[i] == tl_ZbiorZnakow[j])
-			{	//cout<<"literka : "<< tl_ZbiorZnakow[j] <<endl;
-				vl_ZnalezionaLiterka++;
-				exit;}
+	//char tl_ZbiorZnakow[88]={"ABCDEFGHIJKLMNOPQRSTUVWXYZĄĆĘŁŃÓŚŻŹabcdefghijklmnopqrstuvwxyząćęłńóśżź"};
+	string v_szukanyZnak="0123456789!@#$%^&*()_+={}[]:\";'<>,.?/|\'\\";
+	size_t znalezionaPozycja = v_name.find( v_szukanyZnak );
+	if( znalezionaPozycja != std::string::npos )
+		{	cout<< "Wpisane imie jest niepoprawne. Sprobuj jeszcze raz.";
+			return false;
 		}
-	}
-	//cout<<"vl_ZnalezionaLiterka: "<<vl_ZnalezionaLiterka<<endl;
-	if (v_name.length() == vl_ZnalezionaLiterka)
-		{	//getch();
-			return true;
-		}
-	else
-		{cout<< "Wpisane imie jest niepoprawne. Spróbuj jeszcze raz.";}
-return false;
+return true;
 }
 //-------------------------------------------------------------------------------------
 string f_ImieInitCap ( string v_name )
@@ -614,6 +618,31 @@ void f_ObslugaAgregatow()
 }
 /*praca na agregatach*/
 //-------------------------------------------------------------------------------------
+void f_ZapiszTabliceDoPliku ()
+{	int sizeof_tFunboy = sizeof(tFunboy)/sizeof(tFunboy[0]);
+	fstream plik;
+	plik.open( "zodiak.txt", ios::out | ios::app );
+		//cout<<"zapisuje do pliku"<<endl;
+		for (int i=0; i<sizeof_tFunboy; i++)
+		{	//plik<<i<<endl;
+			if (tFunboy[i].imie!="" && tFunboy[i].rok_urodzenia>0 && tFunboy[i].nazwa_dnia_urodzenia!="")
+			{	plik<<tFunboy[i].imie<<';';
+				plik<<tFunboy[i].rok_urodzenia<<';';
+				plik<<tFunboy[i].miesiac_urodzenia<<';';
+				plik<<tFunboy[i].dzien_urodzenia<<';';
+				plik<<tFunboy[i].nazwa_dnia_urodzenia<<';';
+				plik<<tFunboy[i].znak_zodiaku<<';';
+				plik<<tFunboy[i].plec<<';';
+				plik<<tFunboy[i].horoskop_link<<endl;
+			}
+			
+		}	
+			plik.flush();
+			plik.close();
+		cout<<"zapisalem do pliku"<<endl;
+
+}
+//-------------------------------------------------------------------------------------
 
 void f_PodajDane ()
 {
@@ -626,7 +655,7 @@ void f_PodajDane ()
 	{ 	cin.clear();
 		cin.sync();
 		system("cls");
-		cout<<"podaj rok urodzenia" <<endl;
+		cout<<"Podaj rok urodzenia" <<endl;
 		cin>>v_year;
 		v_czyPoprawneDane = f_CzyJestYearUrodzenia( v_year );
 	} while (v_czyPoprawneDane == false);
@@ -635,7 +664,7 @@ void f_PodajDane ()
 	do
 	{ 	cin.clear();
 		cin.sync();
-		cout<<"podaj miesiąc urodzenia" <<endl;
+		cout<<"Podaj miesiąc urodzenia" <<endl;
 		cin>>v_month;
 		v_czyPoprawneDane = f_CzyJestMountUrodzenia( v_month );
 	} while (v_czyPoprawneDane == false);
@@ -649,7 +678,7 @@ void f_PodajDane ()
 	do
 	{	cin.clear();
 		cin.sync();
-		cout<<"podaj dzień urodzenia" <<endl;
+		cout<<"Podaj dzień urodzenia" <<endl;
 		cin>>v_day;
 		v_czyPoprawneDane = f_CzyJestDayUrodzenia( v_day, v_month, v_year);
 		//cout << "v_czyDay: "<<v_czyDay << endl;
@@ -661,60 +690,190 @@ void f_PodajDane ()
 		cin.sync();
 		cout<<"Podaj imię" <<endl;
 		cin>>v_name;
+		cout<<"imie :"<<v_name<<endl;
+	    int n = v_name.length();
+	    char v_char_name[n + 1];
+    
+		strcpy(v_char_name, v_name.c_str()); 
+		cout<<"imie :"<<v_name<<endl;
+		v_name=f_PolskieZnaki(v_char_name);
+		cout<<"imie :"<<v_name<<endl;
 		v_czyPoprawneDane = f_CzyPoprawneImie( v_name) ;	//wywolanie funkcji do walidacji imienia czy np. user nie wpisal 1234...
+		getch();
 	} while (v_czyPoprawneDane== false);
 	v_name = f_ImieInitCap(v_name);							//initcap - niezaleznie od tego co user wpisal.
 	v_dzienUrodzin = f_OdgadnijDzienUrodzin(v_day, v_month, v_year);
 	v_gender = f_SprawdzPlec(v_name);
 	v_zodiac = f_Zodiak(v_day, v_month);
 
-	pFunboy = &Funboy;
-	pFunboy->imie=v_name;
-	pFunboy->rok_urodzenia=v_year;
-	pFunboy->miesiac_urodzenia=v_month;
-	pFunboy->dzien_urodzenia=v_day;
-	pFunboy->nazwa_dnia_urodzenia=v_dzienUrodzin;
-	pFunboy->znak_zodiaku=v_zodiac;
-	pFunboy->plec=v_gender;
-	pFunboy->horoskop_link=f_HoroskopLink(v_zodiac);
+	oFunboy.imie=v_name;
+	oFunboy.rok_urodzenia=v_year;
+	oFunboy.miesiac_urodzenia=v_month;
+	oFunboy.dzien_urodzenia=v_day;
+	oFunboy.nazwa_dnia_urodzenia=v_dzienUrodzin;
+	oFunboy.znak_zodiaku=v_zodiac;
+	oFunboy.plec=v_gender;
+	oFunboy.horoskop_link=f_HoroskopLink(v_zodiac);
 	pFunboy->faza_ksiezyca=f_FazaKsiezycaSlownie(f_Fazaksiezyca(pFunboy->rok_urodzenia, pFunboy->miesiac_urodzenia, pFunboy->dzien_urodzenia));
+	ptFunboy[0]=oFunboy;		//przypisanie wskaźnikowi wartosci
 
 	cin.clear();
 	cin.sync();
 	system("cls");
-	
-	cout << "Cześć "<< pFunboy->imie <<". "<<endl;
-	cout << "Data Twoich narodzin to: " <<pFunboy->dzien_urodzenia<<"/"<<pFunboy->miesiac_urodzenia<<"/"<<pFunboy->rok_urodzenia<<endl;
-	cout << "Dzień Twoich urodzin to: " << pFunboy->nazwa_dnia_urodzenia <<endl;
-	cout << "Twoja płeć to: " <<f_PlecSlownie(pFunboy->plec) << endl;
-	cout << "Twój znak zodiaku to: " << pFunboy->znak_zodiaku << endl;
-	cout << "W dniu Twoich urodzin była następująca faza księżyca: " << pFunboy->faza_ksiezyca << endl; 
-	cout << "Link do Twojego horoskopu: " << f_HoroskopLink(v_zodiac) << endl;
+	cout << "Cześć "<< oFunboy.imie <<". "<<endl;
+	cout << "Data Twoich narodzin to: " << oFunboy.dzien_urodzenia<<"/"<<oFunboy.miesiac_urodzenia<<"/"<<oFunboy.rok_urodzenia<<endl;
+	cout << "Dzień Twoich urodzin to: " << oFunboy.nazwa_dnia_urodzenia <<endl;
+	cout << "Twoja płeć to: ";
+	cout << "Twój znak zodiaku to: " << oFunboy.znak_zodiaku << endl;
+	cout << "W dniu Twoich urodzin była następująca faza księżyca: " << oFunboy.faza_ksiezyca << endl; 
+	cout << "Link do Twojego horoskopu: " << oFunboy.horoskop_link << endl;
 	cout << "Kliknij dowolny przycisk, żeby otworzyć horoskop w przeglądarce." << endl;
+	f_ZapiszTabliceDoPliku();
 	getch();
 	f_OtworzLinkHoroskop(v_zodiac);
 
 	cout << "Press any key to continue... " <<endl;
 	getch();
 }
-
 //-------------------------------------------------------------------------------------
-void f_Zapisz_do_pliku ()
-{
+bool f_Wytnij_Zapisz(int vl_lp, string vl_linia)
+{	
+	string vl_birthdate, vl_day, vl_month, vl_year, vl_name, vl_dzienUrodzin, vl_zodiac;;
+	char vl_separator=';', vl_separator1='-', vl_gender;
+	int t_seperatory[2], vl_licznikSeparatorow =0, i, v_day, v_month, v_year, vl_blad;
+	bool vl_CzyPoprawneDane;
+	//pFunboy = &oFunboy;			//przypisanie wskaźnikowi wartosci
+	
+	for (i=0; i<vl_linia.length()-1; i++) //powtarzaj dopoki i<dlugosci linii
+	{	if (vl_linia[i]==vl_separator)
+		{	t_seperatory[vl_licznikSeparatorow]=i; //zbieranie informacji o pozycji separatora
+			vl_licznikSeparatorow++;} 
+	}
+	if (vl_licznikSeparatorow!=2)
+		return false;
+	
+	vl_name = vl_linia.substr(t_seperatory[0]+1,t_seperatory[1]-t_seperatory[0]-1);
+	vl_birthdate=vl_linia.substr(t_seperatory[1]+1,vl_linia.length()-1);
+	//cout<< "vl_imie : "<<vl_name<<" vl_birthdate : "<<vl_birthdate<<endl;
+	
+	vl_licznikSeparatorow=0;
+	for (i=0; i<vl_birthdate.length()-1; i++) //powtarzaj dopoki i<dlugosci vl_birthdate
+	{	if (vl_birthdate[i]==vl_separator1)
+		{	t_seperatory[vl_licznikSeparatorow]=i; //zbieranie informacji o pozycji separatora1
+			vl_licznikSeparatorow++;} 
+	}
+	vl_year	= vl_birthdate.substr(0,t_seperatory[0]);
+	vl_month= vl_birthdate.substr(t_seperatory[0]+1,t_seperatory[1]-t_seperatory[0]-1);
+	vl_day	= vl_birthdate.substr(t_seperatory[1]+1,vl_birthdate.length()-1);
+	
+	//cout<< "vl_year : "<<vl_year<<" vl_month : "<<vl_month<<" vl_day : "<<vl_day<<endl;
+
+	sscanf(vl_year.c_str(), "%d", &v_year);
+	sscanf(vl_month.c_str(), "%d", &v_month);
+	sscanf(vl_day.c_str(), "%d", &v_day);
+	
+	//sprawdzam poprawnosc danych	
+	vl_blad = 0;
+	vl_CzyPoprawneDane = f_CzyJestYearUrodzenia( v_year );
+	vl_blad = (vl_CzyPoprawneDane + vl_blad );	
+	vl_CzyPoprawneDane = f_CzyJestMountUrodzenia( v_month );
+	vl_blad = (vl_CzyPoprawneDane + vl_blad );	
+	vl_CzyPoprawneDane = f_CzyJestDayUrodzenia( v_day, v_month, v_year);
+	vl_blad = (vl_CzyPoprawneDane + vl_blad );
+	vl_CzyPoprawneDane = f_CzyPoprawneImie( vl_name) ;		//wywolanie funkcji do walidacji imienia czy np. user nie wpisal 1234...
+	vl_blad = (vl_CzyPoprawneDane + vl_blad );
+	//koniec sprawdzania poprawnosci danych	
+	
+	if (vl_blad==4)
+	{	vl_name = f_ImieInitCap(vl_name);						//initcap - niezaleznie od tego co user wpisal.	
+		vl_dzienUrodzin = f_OdgadnijDzienUrodzin(v_day, v_month, v_year);
+		vl_gender = f_SprawdzPlec(vl_name);
+		vl_zodiac = f_Zodiak(v_day, v_month);
+		
+		
+		oFunboy.imie=vl_name;
+		oFunboy.rok_urodzenia=v_year;
+		oFunboy.miesiac_urodzenia=v_month;
+		oFunboy.dzien_urodzenia=v_day;
+		oFunboy.nazwa_dnia_urodzenia=vl_dzienUrodzin;
+		oFunboy.znak_zodiaku=vl_zodiac;
+		oFunboy.plec=vl_gender;
+		oFunboy.horoskop_link=f_HoroskopLink(vl_zodiac);
+		
+		ptFunboy[vl_lp]=oFunboy;
+		//cout<<ptFunboy[vl_lp].imie<<";"<<ptFunboy[vl_lp].znak_zodiaku<<endl;
+
+		return true;
+	}
+	//cout<<"eof"<<endl;
+	return false;
+
+}
+//-------------------------------------------------------------------------------------
+bool f_Import_z_pliku ()
+{	string lv_filename, vl_linia, vl_rozszezenie =".csv";
 	fstream plik;
-	pFunboy = &Funboy;
-	plik.open( "zodiak.txt", ios::out | ios::app );
-		plik<<pFunboy->imie<<';';
-		plik<<pFunboy->rok_urodzenia<<';';
-		plik<<pFunboy->miesiac_urodzenia<<';';
-		plik<<pFunboy->dzien_urodzenia<<';';
-		plik<<pFunboy->nazwa_dnia_urodzenia<<';';
-		plik<<pFunboy->znak_zodiaku<<';';
-		plik<<pFunboy->plec<<';';
-		plik<<pFunboy->faza_ksiezyca<<';';
-		plik<<pFunboy->horoskop_link<<';'<<endl;
-		plik.flush();
+	int vl_Lp =0;
+	bool vl_CzyPoprawneDane;
+	pFunboy = &oFunboy;
+	int vl_sizeof_tFunboy = sizeof(tFunboy)/sizeof(tFunboy[0]);
+	cin.clear();
+	cin.sync();
+	system("cls");
+	
+	cout<< "Import danych z pliku csv do glownego pliku danych"<<endl;
+	cout<< "Plik z danymi do zaimportowania musi zawierac 3 kolumny: "<<endl;
+	cout<< "nr kolejny, imie i data urodzenia w formacie yyyy-mm-dd. "<<endl;
+	cout<< "Kolumny musza byc oddzielone od siebie ';' "<<endl;
+	cout<< "Podaj nazwe pliku .csv z danymi do zaimportowania: ";
+	cin>> lv_filename;
+
+	size_t znalezionaPozycja = lv_filename.find( vl_rozszezenie );
+	if( znalezionaPozycja == std::string::npos )
+	{	lv_filename=lv_filename+vl_rozszezenie;}
+	
+		//cout << lv_filename << endl;
+
+	plik.open( lv_filename.c_str(), ios::in );
+	if( plik.good() == false )
+    {	cout << "Plik nie istnieje."<<endl;
+    	getch();
+		return false;}
+
+	while(!plik.eof())
+		{	
+			getline(plik, vl_linia);
+			if (vl_linia.length()!=0)
+			{	
+				vl_CzyPoprawneDane = f_Wytnij_Zapisz(vl_Lp, vl_linia);
+				if (vl_CzyPoprawneDane==true)
+				{
+					if (vl_Lp < vl_sizeof_tFunboy-1)
+					{ vl_Lp++;}	
+					else
+					{ 	f_ZapiszTabliceDoPliku();
+						vl_Lp=0;
+						//zerowanie tablicy ptFunboy[vl_lp]=oFunboy;
+						//zeruje atrybuty obiektu	
+						for (int i=0; i<vl_sizeof_tFunboy; i++)
+						{	//plik<<i<<endl;
+							tFunboy[i].imie="";
+							tFunboy[i].rok_urodzenia=0;
+							tFunboy[i].miesiac_urodzenia=0;
+							tFunboy[i].dzien_urodzenia=0;
+							tFunboy[i].nazwa_dnia_urodzenia="";
+							tFunboy[i].znak_zodiaku="";
+							tFunboy[i].horoskop_link="";
+						}
+					}
+				}
+			}
+		}
+	f_ZapiszTabliceDoPliku();
+//	delete tFunboy;
 	plik.close();
+    getch();
+return true;
 }
 //-------------------------------------------------------------------------------------
 void f_OdczytPelnejBazy()
@@ -738,10 +897,11 @@ void f_OdczytPelnejBazy()
 
 int main()
 {	//srand(time ( 0 ) );
-	setlocale(LC_CTYPE, "Polish");
+	setlocale(LC_CTYPE, "pl_PL");
 	Cechy *pFunboy; //deklaracja wskaznika do obiektu
 	char v_znak;
-	pFunboy = &Funboy;
+	pFunboy = &oFunboy;
+	bool v_Import_z_pliku;
 
 	do
 	{
@@ -785,7 +945,7 @@ int main()
 		cout << "   Określenie płci na podstawie imienia		"	<< endl;
 		cout << "   Wyznaczenie wieku użytkownika				"	<< endl;
 		cout << "   Rozpoznanie znaku zodiaku					"	<< endl;
-		cout << "   Wyswietl horoskop							"	<< endl;
+		cout << "   Wyświetl horoskop							"	<< endl;
 		cout << "   Wyznaczenie fazy księżyca w dniu urodzin	"	<< endl;
 		cout << "2. Wyswietl historię							"	<< endl;
 		cout << "3. Dodaj dane z pliku							"	<< endl;
@@ -798,7 +958,7 @@ int main()
 		{
 			case '1':
 				f_PodajDane();
-				f_Zapisz_do_pliku();
+//				f_Zapisz_do_pliku();
 
 				cin.clear();
 				cin.sync();
