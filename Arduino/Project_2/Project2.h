4c8fb3fa15ebf4a32e20a5802bff5f0feb2c086c
Tomasz Jan Oksiędzki <oksiedz@gmail.com>
2020-07-08 21:43:12 +0200

    Projekt 2 na Programowanie w c++ mikrokontrolerow

--- Arduino/Project_2/Project2.h
+++ Arduino/Project_2/Project2.h
@@ -1,121 +1,118 @@
-//biblioteki do wyswietlacza start
-#include <Adafruit_GFX.h>
-#include <Adafruit_MonoOLED.h>
-#include <Adafruit_SPITFT.h>
-#include <Adafruit_SPITFT_Macros.h>
-#include <gfxfont.h>
+
+//biblioteka do urzadzen i2c
 #include <Wire.h>
-#include <Adafruit_SSD1306.h>
-#include <splash.h>
-//bitblioteki do wyswietlacza koniec
-#include <Keypad.h> //biblioteka od klawiatury
+
+#include <SPI.h>
 
 //biblioteki do czujnika temperatury, wilgotnosci i cisnienia
-#include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+//biblioteki do oled
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//inicjalizacja biblioteki OLED
+Adafruit_SSD1306 display(4); // unused pin
+
+//definicja adresu czujnika temperatury, wilgotnosci i cisnienia
+#define BME280_I2C_ADDRESS  0x76
+
+//inicjalizacja biblioteki czujnika temperatury, wilgotnosci, cisnienia
+Adafruit_BME280 bme; // hardware SPI
 
 
-// OLED display TWI address
-#define OLED_ADDR   0x3C
-
-Adafruit_SSD1306 display(-1);
-
-#if (SSD1306_LCDHEIGHT != 64)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
-
-
-//definiowanie pod czujnik temperatury
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
-
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
-
-unsigned long delayTime;
+//definicja zmiennych
+const int LEDpin = 13;
+const int delayTime = 1000;
+const float kryt_dyst = 10;
+//definiowanie pinow pod czujnik dystansu
+const int trigPin = 12;
+const int echoPin = 11;
 
 
+void setup() {
 
-const byte ROWS = 4; //ile wierszy
-const byte COLS = 4; //ile kolumn
+ //wlaczenie pinow
+ pinMode (trigPin, OUTPUT);
+ pinMode (echoPin, INPUT);
+ pinMode (LEDpin, OUTPUT);
 
-byte rowPins[ROWS] = {9, 8, 7, 6};
-byte colPins[COLS] = {5, 4, 3, 2};
-
-char keys[ROWS][COLS] = {//mapowanie klawiatury
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
-
-Keypad klawiatura = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);//inicjalizacja klawiatury
-
-void i_a_c_display()
-{
-  
-}
-
-void button(char klawisz)
-{
-  display.clearDisplay();
-  display.display();
-  display.setCursor(0, 0);
-  display.print("Wybrales ");
-  display.print(klawisz);
-  display.display();
-}
-
-void setup()
-{
-  Serial.begin(9600);//do tworzenia w monitorze systemowym
-
-
-
-
-
-  // initialize and clear display
-  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
-  display.clearDisplay();
-  display.display();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  // display a line of text
-  display.setCursor(0, 0);
-  display.print("Witaj w programie Artura i Tomka.");
-  delay(3000);
-  display.setCursor(0, 20);
-  display.print("Wybierz jeden z przyciskow A, B, C, D.");
-  // update display with all of the above graphics
-  display.display();
-}
-
-
-void loop()
-{
-  char klawisz = klawiatura.getKey();
-  if (klawisz) {
-    Serial.println(klawisz);
-  };
-  if (klawisz == 'A') {
-    button(klawisz);
-  };
-  if (klawisz == 'B') {
-    button(klawisz);
-  };
-  if (klawisz == 'C') {
-    button(klawisz);
-  };
-  if (klawisz == 'D') {
-    button(klawisz);
-  };
-
+ //debug w serial i sprawdzenie statusow czujnika
+ Serial.begin(9600);
+ while (!Serial);
+ Serial.println(" ");
+ if (bme.begin(BME280_I2C_ADDRESS) == 0) {
+ Serial.println("CNo valid BME280 sensor, check wiring, address, sensor ID!");
+ while (1) delay(10);
  }
-  
+ display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+ display.clearDisplay();
+ display.display();
+}
+
+void loop() {
+ //odczytanie wskazan i zapisanie do zmiennych
+ float temp = bme.readTemperature();
+ float wilg = bme.readHumidity();
+ float dyst = dystans_cm(temp, wilg);
+ //zrzucenie wartosci do debuga
+ debug(temp, wilg, dyst);
+ //zrzucenie wynikow na ekran
+ wyswietl(temp, wilg, dyst);
+ //wlaczenie diody gdy obiekt zblizy sie za mocno
+ if (dyst<kryt_dyst)
+     {
+       for(int i = 0; i < 10; i++)
+       {
+        digitalWrite(LEDpin, HIGH);
+        delay(100);
+        digitalWrite(LEDpin, LOW);
+        delay(100);
+       };
+     }
+ 
+ delay(delayTime);
+}
+void debug(float temp, float wilg, float dyst) {
+ Serial.print("Temperatura = ");
+ Serial.print(temp);
+ Serial.println(" *C");
+ Serial.print("Wilgotnosc = ");
+ Serial.print(wilg);
+ Serial.println(" %");
+ Serial.print("Dystans = ");
+ Serial.print(dyst);
+ Serial.println(" cm");
+ Serial.println ( );
+}
+void wyswietl (float temp, float wilg, float dyst) {
+ display.clearDisplay();
+ display.setTextSize(1);
+ display.setTextColor(WHITE);
+ display.setCursor(0, 0);
+ display.print("Temperatura ");
+ display.print(temp);
+ display.print(" C");
+ display.setCursor(0, 12);
+ display.print("Wilgotnosc ");
+ display.print(wilg);
+ display.print(" %");
+ display.setCursor(0, 25);
+ display.print("Dystans ");
+ display.print(dyst);
+ display.print(" cm");
+ display.display();
+}
+float dystans_cm (float temp, float wilg) {
+ digitalWrite (trigPin, LOW);
+ delayMicroseconds (2);
+ digitalWrite (trigPin, HIGH);
+ delayMicroseconds (10);
+ digitalWrite (trigPin, LOW);
+ delayMicroseconds (2);
+ int czasPulsu_us = pulseIn (echoPin, HIGH);
+ float dzwiek_m_sec = 331.4 + (.606 * temp) + (.0124 * wilg);
+ float dzwiek_cm_us = dzwiek_m_sec / 10000.0;
+ float dystans_cm = (dzwiek_cm_us * czasPulsu_us) / 2;
+ return dystans_cm;
+}
